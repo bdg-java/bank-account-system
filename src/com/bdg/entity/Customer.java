0927d0251f224a9d442290cca440566301d11ae1
Seda Hovsepyan <sedahovsepyan8@gmail.com>
2019-06-25 22:55:04 +0400

    Some part is ready....

---
+++ src/com/bdg/entity/Customer.java
@@ -0,0 +1,24 @@
+package com.bdg.entity;
+
+public class Customer {
+    private String customerName;
+    private String customerSurName;
+    private Address customerAddress;
+
+    public Customer( String customerName, String customerSurName, Address customerAddress){
+        this.customerName = customerName;
+        this.customerSurName = customerSurName;
+        this.customerAddress = customerAddress;
+    }
+
+
+
+    public String toString(){
+        return "Customer : {" +
+                "CustomerName :"+
+                customerName + "," +
+                "CustomerSurName :" + customerSurName +","+
+                "customerAddress :" + customerAddress+
+                "}";
+    }
+}
